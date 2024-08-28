65f27a43b44135e666e5144d6da100544882e9c2
陈远翔 <cyxhuan9@163.com>
2024-08-28 19:57:01 +0800

    add prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  build: {
-    transpile: ['trpc-nuxt'],
-  },
+  modules: ['@prisma/nuxt'],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
-const { $client } = useNuxtApp()
-
-const hello = await $client.hello.useQuery({ text: 'client' })
-const greeting = computed(() => hello.data.value)
 </script>
 
 <template>
-  <div>
-    <p>{{ greeting?.greeting }}</p>
-  </div>
+  <div />
 </template>
