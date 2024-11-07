5f326d8f506b5d70cc08b88ee2a15cd4d95be531
陈远翔 <cyxhuan9@163.com>
2024-11-07 23:26:32 +0800

    add logto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import process from 'node:process'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
@@ -8,11 +7,18 @@ export default defineNuxtConfig({
     // The private keys which are only available within server-side
     databaseUrl: process.env.DATABASE_URL,
     shaSecret: process.env.SHA_SECRET,
+    logto: {
+      endpoint: process.env.NUXT_LOGTO_ENDPOINT,
+      appId: process.env.NUXT_LOGTO_APP_ID,
+      appSecret: process.env.NUXT_LOGTO_APP_SECRET,
+      cookieEncryptionKey: process.env.NUXT_LOGTO_COOKIE_ENCRYPTION_KEY,
+    },
     // Keys within public, will be also exposed to the client-side
     public: {},
   },
   modules: [
     '@prisma/nuxt',
+    '@logto/nuxt',
   ],
   prisma: {
     installCLI: true,
@@ -21,5 +27,13 @@ export default defineNuxtConfig({
     formatSchema: true,
     installStudio: true,
     autoSetupPrisma: true,
+  },
+  logto: {
+    customRedirectBaseUrl: 'http://localhost:3000',
+    pathnames: {
+      signIn: '/auth/sign-in',
+      signOut: '/auth/sign-out',
+      callback: '/auth/callback',
+    },
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const user = useLogtoUser()
+</script>
+
 <template>
-  <div>home</div>
+  <!-- Display user information when signed in -->
+  <ul v-if="Boolean(user)">
+    <li v-for="(value, key) in user" :key="key">
+      <b>{{ key }}:</b> {{ value }}
+    </li>
+  </ul>
+  <!-- Simplified button for sign-in and sign-out -->
+  <a :href="`/auth/sign-${user ? 'out' : 'in'}`"> Sign {{ user ? 'out' : 'in' }} </a>
 </template>
